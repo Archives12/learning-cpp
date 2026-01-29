5f5856034bbfd9f4ae556403909ae783601c3f0e
Priya Mehta <pmehta12122005@gmail.com>
2026-01-29 13:55:35 +0530

    Last and First Digit

---
+++ Pointer/LD_FD.cpp
@@ -0,0 +1,22 @@
+/* Write a function to find out the first and last digit of a number without returning anything*/
+
+#include<iostream>
+using namespace std;
+void find(int n, int* ptr1, int* ptr2){
+    *ptr2 = n%10;  //Last Digit
+    while (n>9){
+    n/=10;  //First Digit
+    }
+
+    *ptr1 = n; 
+    return;
+}
+int main(){
+    int n;
+    cin>>n;
+    int firstDigit, lastDigit;
+    int* ptr1 = &firstDigit;
+    int* ptr2 = &lastDigit;
+    find(n,ptr1, ptr2);
+    cout<<firstDigit << " " <<lastDigit;
+}
