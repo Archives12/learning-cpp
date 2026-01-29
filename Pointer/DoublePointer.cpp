622a2f815c8144a7631c039348a7d4262bae6feb
Priya Mehta <pmehta12122005@gmail.com>
2026-01-29 14:03:41 +0530

    Double Pointer

---
+++ Pointer/DoublePointer.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    int* ptr = &x;
+    int** p = &ptr;
+    cout << x <<endl;           //5
+    cout << *ptr <<endl;        //5
+    cout << **p <<endl;         //5
+    cout << &x <<endl;         //0x61ff08
+    cout << ptr <<endl;        //0x61ff08
+    cout << * p <<endl;        //0x61ff08
+}
